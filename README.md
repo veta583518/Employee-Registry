4172906bfcf8c0cead95e73ebb75f47e7792b325
CoCo Chanel <veta583518@gmail.com>
2021-05-20 18:41:09 -0500

    fixed generate card now displaying

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
-# TeamProfileGenerator
+# Employee Registry
 
 ## Description
 
 Application that takes in information about employees and generates an HTML webpage that displays summaries for each person.
+
+---
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Testing](#testing)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+---
+
+## Installation
+
+To install, clone/download the repo. Open your terminal and navigate to the root directory of where you saved the repo and run `npm install` to install the dependencies.
+
+---
+
+## Usage
+
+To start this application run `node index` from your root directory. This is a CLI application and most of your interaction will take place at the command line. Once the application is started you will be prompted as the manager to answer some questions about yourself. Then you have the option to build your team. You will be asked the role of the employee that you wish to add to your team. From there you will be prompted with questions about wach team member that you add. When you have completed entering all of your team members select build team. This will generate your team's profile. Enjoy!
+
+---
+
+## Testing
+
+To test run `npm test`
+
+---
+
+## Contributing
+
+This application is not open for contribution at this time
+
+---
+
+## Questions
+
+You may contact me via the following if you have any questions:
+
+- GitHub: [veta583518](www.github.com/veta583518)
+- Email: [veta583518](mailto:veta583518@gmail.com)
--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -3,27 +3,29 @@
 // const generateIntern = require("../src/generateIntern");
 
 // create card for each employee
+
 const generateCards = (teamData) => {
-  const htmlArray = [];
-  // teamData.map((name, id, email, icon, conditional, role) => {
-  //   return `
-  //   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
-  //     <div class="card-header bg-primary">
-  //       <h2 class="card-title text-white"> ${name}</h2>
-  //       <h3 class="card-subtitile mb-2 text-white"><i class="${icon} mr-3"></i>${role}</h3>
-  //     </div>
-  //     <div class="card-body">
-  //       <ul class="list-group list-group-flush">
-  //         <li class="list-group-item p3">Employee ID: ${id}</li>
-  //         <li class="list-group-item p3">Email Address:
-  //           <a href = "mailto:${email}"> ${email}</a>
-  //         </li>
-  //         ${conditional}
-  //       </ul>
-  //     </div>
-  //   </div>
-  //   `;
-  // });
+  let htmlArray = [];
+  teamData.map((employee) => {
+    const html = `
+    <div class="card shadow mb-5 bg-body rounded" style="width: 18rem;">
+      <div class="card-header bg-primary">
+        <h2 class="card-title text-white"> ${employee.name}</h2>
+        <h3 class="card-subtitile mb-2 text-white"><i class="${employee.icon}" id="icon"></i>${employee.role}</h3>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item p3">Employee ID: ${employee.id}</li>
+          <li class="list-group-item p3">Email Address:
+            <a href = "mailto:${employee.email}"> ${employee.email}</a>
+          </li>
+          ${employee.conditional}
+        </ul>
+      </div>
+    </div>
+    `;
+    return (htmlArray = [...htmlArray, html]);
+  });
 
   return htmlArray.join(" ");
 };
@@ -39,7 +41,7 @@ module.exports = generatePage = (teamData) => {
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <title>Team Profile Generator</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         </head>
       
