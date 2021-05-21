1405cc8aef82622ccf30f7b7da48e4a9cfffa7b8
CoCo Chanel <veta583518@gmail.com>
2021-05-20 19:40:24 -0500

    corrected some margins

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -8,10 +8,10 @@ const generateCards = (teamData) => {
   let htmlArray = [];
   teamData.map((employee) => {
     const html = `
-    <div class="card shadow mb-5 bg-body rounded" style="width: 18rem;">
+    <div class="card shadow mb-5 me-3 bg-body rounded" style="width: 18rem;">
       <div class="card-header bg-primary">
         <h2 class="card-title text-white"> ${employee.name}</h2>
-        <h3 class="card-subtitile mb-2 text-white"><i class="${employee.icon}" id="icon"></i>${employee.role}</h3>
+        <h3 class="card-subtitile mb-2 text-white"><i class="${employee.icon} me-2"></i>${employee.role}</h3>
       </div>
       <div class="card-body">
         <ul class="list-group list-group-flush">
@@ -42,7 +42,7 @@ module.exports = generatePage = (teamData) => {
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <title>Team Profile Generator</title>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         </head>
       
         <body>
@@ -51,15 +51,18 @@ module.exports = generatePage = (teamData) => {
           </header>
     
           <main class="d-flex justify-content-center p-2">
-              <div class="col-sm d-flex justify-content-center flex-wrap p-2">
+          <div class="col-sm-6 g-4">
+              <div class="d-flex justify-content-center flex-wrap p-2">
                 ${generateCards(teamData)}
               </div>
+          </div>
           </main>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         </body>
       </html>
     `;
     if (teamHTML) {
+      console.log("...Generating Team Registry");
       resolve(teamHTML);
     } else {
       reject();
